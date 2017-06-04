b982ea171984eb63aa03ed8f143e9db9be0ace1d
czx95110 <czx95110@163.com>
2017-06-04 22:07:11 +0800

    third

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,20 @@ var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var plugin = gulpLoadPlugins();
 
+//babel + rename 综合 基本用法
 gulp.task('js', function () {
   gulp.src('srcjs/es6.js')
-    .pipe()
-    .pipe()
+    .pipe(plugin.babel({
+      presets: ['es2015']
+    }))
+    .pipe(plugin.rename({
+        //dirname:'js/abc',
+        basename: 'es6.transfer',
+        //prefix:'bons-',
+        //suffix:'-hola',
+        extname: '.js'
+      }
+    ))
     .pipe(gulp.dest('destjs'));
+  //the inalName is : destjs/js/abc/bons-es6.transfer-hola.js
 });
--- srcjs/es6.js
+++ srcjs/es6.js
@@ -1,3 +1,6 @@
-
-
-
+var arr = [1, 9, 2, 3, 1, 9, 4, 8, 5, 3, 7, 8, 9, 5];
+console.log(arr);
+arr.sort((a, b)=> {
+  return b - a;
+});
+console.log(arr);
